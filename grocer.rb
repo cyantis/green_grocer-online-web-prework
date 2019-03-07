07530bd2881f5ba3e893a162ac5e2b5d17ab9d84
Cody Yantis <cyantis@gmail.com>
2019-03-07 02:15:12 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,31 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  cart.each do |hash|
+    hash.each do |item, info|
+      if !consolidated_cart[item]
+        consolidated_cart[item] = info
+        consolidated_cart[item][:count] = 1
+      else
+        consolidated_cart[item][:count] += 1
+      end
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+# code here
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |hash, item|
+    if item[:clearance]
+      new_price = item[:price] * 0.80
+      item[:price] = new_price.round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
