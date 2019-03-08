0822784970eeb2c92fe84e187cb9796b4cefdaa0
Cody Yantis <cyantis@gmail.com>
2019-03-08 23:18:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def checkout(cart, coupons)
   total_arr = clearance_cart.collect do|c|
     c[1][:price]
   end
-  total = total_arr.sum {|num| num}
+  total = total_arr.sum
   total *= 0.90 if total > 100.00
   total
 end
