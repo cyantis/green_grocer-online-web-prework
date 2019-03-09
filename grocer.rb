bde6b2a0f079cf40f56fb6601ac8248761d6cee0
Cody Yantis <cyantis@gmail.com>
2019-03-09 00:01:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
       if cart["#{name} W/COUPON"]
         cart["#{name} W/COUPON"][:count] += 1
       else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost], :clearance => cart[name][:clearance]}
+        cart["#{name} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[name][:clearance], :count => 1}
       end
       cart[name][:count] -= coupon[:num]
     end
